395e4eb10281e5669b5358a83e5fdaf5da7e4f48
jjeffrin <jjeffrin@gmail.com>
2019-07-30 12:35:45 +0530

    Final Commit

--- FactoryMethodDesignPattern/Program.cs
+++ FactoryMethodDesignPattern/Program.cs
@@ -2,11 +2,60 @@
 
 namespace FactoryMethodDesignPattern
 {
+    public interface IFactory
+    {
+        void Drive(int miles);
+    }
+
+    public class Scooter : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+        }
+    }
+
+    public class Bike : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+        }
+    }
+
+    public abstract class VehicleFactory
+    {
+        public abstract IFactory GetVehicle(string Vehicle);
+
+    }
+
+    public class ConcreteVehicleFactory : VehicleFactory
+    {
+        public override IFactory GetVehicle(string Vehicle)
+        {
+            switch (Vehicle)
+            {
+                case "Scooter":
+                    return new Scooter();
+                case "Bike":
+                    return new Bike();
+                default:
+                    throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", Vehicle));
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            VehicleFactory factory = new ConcreteVehicleFactory();
+
+            IFactory scooter = factory.GetVehicle("Scooter");
+            scooter.Drive(10);
+
+            IFactory bike = factory.GetVehicle("Bike");
+            bike.Drive(20);
         }
     }
 }
